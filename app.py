ae6be6e3755b322324e3a14fcbb292848933fef5
Tomáš Pazour <pazour.tomas@student.gymzr.cz>
2026-05-06 12:34:11 +0200

    ukol 7

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def index():
 
 @app.route("/pozdrav-post", methods=["POST", "GET"])
 def pozdrav_post():
+	check=True
+	zprava_jmeno="jmeno splňuje požadavky"
+	zprava="Uhodl jsi heslo!"
 	#aktuální datum
 	date = datetime.now().strftime("%d. %m. %Y")
 
@@ -22,15 +25,25 @@ def pozdrav_post():
 	surname = request.form.get("surname")
 	password = request.form.get("password")
 
+
+	if not name:
+		zprava_jmeno="Error - nelze nezadat jméno"
+		check=False
+
+	elif len(name) > 50:
+		zprava_jmeno="Error - jméno je příliš dlouhé"
+		check=False
+
+
 	
-	if password == "tajneheslo":
+	if password == "tajneheslo" and check==True:
 		zprava="Uhodl jsi heslo!"
 
 	else:
-		zprava="Error"
+		zprava="Error - špatné heslo"
 		
 
-	return render_template("pozdrav_post.html", date=date, name=name, surname=surname, zprava=zprava)
+	return render_template("pozdrav_post.html", date=date, name=name, surname=surname, zprava=zprava, zprava_jmeno=zprava_jmeno)
 
 if __name__=="__main__":
 	app.run(debug=True)
